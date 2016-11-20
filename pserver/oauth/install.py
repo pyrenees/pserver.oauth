6fa610ede783e818857f57ce8ab106f585b5217e
Ramon Navarro Bosch <ramon.nb@gmail.com>
2016-11-20 21:51:53 +0100

    adapting to plone.server 1.0a3

--- pserver/oauth/install.py
+++ pserver/oauth/install.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 from plone.server.addons import Addon
-from plone.server.registry import IAuthExtractionPlugins
-from plone.server.registry import IAuthPloneUserPlugins
+from plone.server import AUTH_USER_PLUGINS
+from plone.server import AUTH_EXTRACTION_PLUGINS
 from plone.server.registry import ILayers
+from pserver.oauth.oauth import PloneJWTExtraction
+from pserver.oauth.oauth import OAuthPloneUserFactory
 
-
-AUTH_EXTRACTION_PLUGIN = 'pserver.oauth.oauth.PloneJWTExtraction'
-AUTH_PLONE_FACTORY_PLUGIN = 'pserver.oauth.oauth.OAuthPloneUserFactory'
 POAUTH_LAYER = 'pserver.oauth.interfaces.IPOAuthLayer'
+
+AUTH_USER_PLUGINS.append(OAuthPloneUserFactory)
+AUTH_EXTRACTION_PLUGINS.append(PloneJWTExtraction)
 
 
 class POauthAddon(Addon):
@@ -18,22 +20,10 @@ class POauthAddon(Addon):
         registry.forInterface(ILayers).active_layers |= {
             POAUTH_LAYER
         }
-        registry.forInterface(IAuthExtractionPlugins).active_plugins |= {
-            AUTH_EXTRACTION_PLUGIN
-        }
-        registry.forInterface(IAuthPloneUserPlugins).active_plugins |= {
-            AUTH_PLONE_FACTORY_PLUGIN
-        }
 
     @classmethod
     def uninstall(self, request):
         registry = request.site_settings
         registry.forInterface(ILayers).active_layers -= {
             POAUTH_LAYER
-        }
-        registry.forInterface(IAuthExtractionPlugins).active_plugins -= {
-            AUTH_EXTRACTION_PLUGIN
-        }
-        registry.forInterface(IAuthPloneUserPlugins).active_plugins -= {
-            AUTH_PLONE_FACTORY_PLUGIN
         }
